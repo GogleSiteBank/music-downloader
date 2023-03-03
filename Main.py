9b530d3c5b84d7cb82a75cc1de7b2dea43e7051e
GogleSiteBank <125816677+GogleSiteBank@users.noreply.github.com>
2023-03-03 17:23:44 +0000

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+import youtube_dl
+from youtube_search import YoutubeSearch as search
+import platform
+import os
+config = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'flac',
+    }]
+}
+
+os.system("") ## Windows is stupid and requires us to run a command to allow ansi codes.
+
+song = (f"https://youtube.com/watch?v=" + str(search(input("\u001b[35mInput song: \u001b[33m"), max_results=10).to_dict()).replace("[{'id': '", "").split("', 't", 1)[0])
+print("\x1b[32m")
+
+try:
+    with youtube_dl.YoutubeDL(config) as down:
+        down.download([song])
+except youtube_dl.utils.DownloadError as e:
+    print(f"\x1b[31mmffmpeg is not installed, please install here: https://ffmpeg.org/")
+except Exception as e:
+    if input("Unkown error, would you like to print it?(y/n): ").lower() == "y":
+        print(f"\x1b[31mERROR BELOW: \n{e}")
+
+
+os.system("color")
+
+if __name__ == '__main__':
+    pass
